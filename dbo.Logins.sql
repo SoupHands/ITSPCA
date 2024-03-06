6f358d28e55c0417301bb9a203d5f638e6086e51
SoupHands <puleoad01@buffalostate.edu>
2024-03-06 11:41:01 -0500

    SQL

---
+++ dbo.Logins.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Logins] (
+    [UserID]     VARCHAR (25) NOT NULL,
+    [Password]   VARCHAR (30) NOT NULL,
+    [TimeIn]     DATETIME     NULL,
+    [TimeOut]    DATETIME     NULL,
+    [WorkDay]    DATE         NULL,
+    [TotalHours] TIME (7)     NULL,
+    PRIMARY KEY CLUSTERED ([UserID] ASC)
+);
+
